2e4402315729f275a76b7d78146a54ba0fb78a17
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2021-07-25 09:41:33 +0300

    Add project files.

---
+++ MyClassLibraryForTDD/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibraryForTDD
+{
+    public class Class1
+    {
+    }
+
+    public class Rooter
+    {
+        public Rooter()
+        {
+        }
+
+        public double SquareRoot(double input)
+        {
+            // Initial code
+            // throw new NotImplementedException();
+            // Another try
+            // return input / 2;
+            // Final try
+            if (input <= 0.0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            double result = input;
+            double previousResult = -input;
+            while (Math.Abs(previousResult - result) > result / 1000)
+            {
+                previousResult = result;
+                // Refactoring
+                result = (result + input / result) / 2;
+                //was: result = result - (result * result - input) / (2*result);
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTestForMyClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestForMyClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestForMyClassLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f970315f-f8e2-43c8-bf1e-ff0ad7e68e23")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
